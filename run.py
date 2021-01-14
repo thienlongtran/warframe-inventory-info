3ddb90a98faf835bbcd1f0d670e6a873b1607150
Thien Tran <19trant7@gmail.com>
2021-01-13 21:31:49 -0600

    Found and added mask values of text

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
+import cv2
 import pytesseract
-from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-print(pytesseract.image_to_data(Image.open('pic.png')))
+img = cv2.imread("pic.png")
+
+"""
+Warframe Text Mask Values;
+Lower HSV: (21, 100, 125)
+Higher HSV: (23, 132, 208)
+"""
+
+lower_gold = (21,100,125)
+higher_gold = (23,132,208)
+
+img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+img = cv2.inRange(img, lower_gold, higher_gold)
+cv2.imshow("Result",img)
+cv2.waitKey(0)
