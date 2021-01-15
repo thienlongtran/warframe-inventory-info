2c48cc930c6571a33249ee780f08e30709e47456
Thien Tran <19trant7@gmail.com>
2021-01-14 21:47:55 -0600

    Separated image parsing function into separate module

---
+++ image_parser.py
@@ -0,0 +1,26 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+def getItemListFromImage(img):
+    img = maskImage(img)
+    collection = pytesseract.image_to_string(img)
+
+    collection = collection.split("\n\n") #Seperate items by empty newline
+    for i in range(len(collection)):
+        collection[i] = collection[i].replace("\n", " ")
+
+    return collection
+
+def maskImage(img):
+    """
+    Warframe Text Mask Values;
+    Lower HSV: (21, 100, 125)
+    Higher HSV: (23, 132, 208)
+    """
+    lower_gold = (21,100,125)
+    higher_gold = (23,132,208)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    img = cv2.inRange(img, lower_gold, higher_gold)
+    return img
--- run.py
+++ run.py
@@ -1,37 +1,8 @@
 import cv2
 import pytesseract
+import image_parser
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 img = cv2.imread("./test_image/pic4.png")
-img = img[0:1080, 80:1400]
-
-cv2.imshow("cropped", img)
-
-"""
-Warframe Text Mask Values;
-Lower HSV: (21, 100, 125)
-Higher HSV: (23, 132, 208)
-"""
-
-lower_gold = (21,100,125)
-higher_gold = (23,132,208)
-
-img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
-#cv2.imshow("Result1", img)
-
-img = cv2.inRange(img, lower_gold, higher_gold)
-
-collection = pytesseract.image_to_string(img)
-collection = collection.split("\n\n") #Seperate items by empty newline
-
-
-for i in range(len(collection)):
-  collection[i] = collection[i].replace("\n", " ")
-
-print(collection)
-cv2.imwrite("Result.jpg", img)
-cv2.imshow("Result", img)
-
-cv2.waitKey(0)
+print(image_parser.getItemListFromImage(img))
