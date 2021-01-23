56d526fb5c9b8718ce996ec959b19d963f69cb41
thienlongtran <19trant7@gmail.com>
2021-01-23 17:43:39 -0600

    Started working on output to excel file

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import cv2
 import pyautogui
 import numpy
+import openpyxl
 from pynput import keyboard
 import image_parser
 
@@ -18,7 +19,16 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         print(list((filter(None,item_list))))
+        itemListToExcel(list((filter(None,item_list))))
         return False
+
+def itemListToExcel(item_list):
+    wb = Workbook()
+    ws = wb.active
+
+    for i in range(len(item_list)):
+        ws["A" + i] = item_list[i]
+
 
 item_list = []
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
