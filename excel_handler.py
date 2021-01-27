5a2a2771fbbe48eba192de42c1088d43bacee64b
Thien Tran <19trant7@gmail.com>
2021-01-27 00:34:27 -0600

    Completed saving to Excel file portion of program.

--- excel_handler.py
+++ excel_handler.py
@@ -1,4 +1,5 @@
 from openpyxl import Workbook
+import warframe_market_script
 
 def itemListToExcel(item_list):
     wb = Workbook()
@@ -8,5 +9,18 @@ def itemListToExcel(item_list):
     ws["A1"] = "Item"
     for i in range(len(item_list)):
         ws["A" + str(i+2)] = item_list[i]
+    
+    ws["B1"] = "Plat"
+    for i in range(len(item_list)):
+        ws["B" + str(i+2)] = str(warframe_market_script.getAveragePlatPrice(ws["A"+ str(i+2)].value))
+
+    ws["C1"] = "Ducat"
+    for i in range(len(item_list)):
+        ws["C" + str(i+2)] = str(warframe_market_script.getDucatPrice(ws["A"+ str(i+2)].value))
+    
+    ws.column_dimensions['D'].width = 10
+    ws["D1"] = "Plat:Ducat"
+    for i in range(len(item_list)):
+        ws["D" + str(i+2)] = "1 : " + str(round((float(ws["C" + str(i+2)].value) / float(ws["B" + str(i+2)].value)),2))
 
     wb.save(filename="result.xlsx")
